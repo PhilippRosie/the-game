51b5f4a3b807d4ebfbb1dab288734c7c05fe0920
“noxar94” <Lundmark1223@hotmail.se>
2023-04-21 15:05:12 +0200

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom"; // Link instead of a
-import { useRef } from "react"; // add a ref attribute to an element to access it directly.
 
 import React from "react"; // react
 import "../styles/navbar.css"; //navbar
@@ -7,17 +6,10 @@ import "../pages/Login";
 import "../pages/Logout";
 import "../pages/game";
 export default function Navbar() {
-  const navRef = useRef();
-
-  const showNavbar = () => {
-    // toggle navbar
-
-    navRef.current.classList.toggle("responsive_nav");
-  };
   return (
     <header>
       <h3 className="logo">OUR GAME</h3>
-      <nav ref={navRef}>
+      <nav>
         <Link to="/">Home</Link>
         <Link to="/login">Login</Link>
         <Link to="/logout">Logout</Link>
