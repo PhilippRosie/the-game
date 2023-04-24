619b243f312f6befe57fe074aca78bbf98c35918
PhilippRosie <122967743+PhilippRosie@users.noreply.github.com>
2023-04-24 16:14:40 +0200

    added background to gamepage

--- src/pages/game.js
+++ src/pages/game.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "../styles/game.css";
+import videoBg from "../assets/game-background.mp4";
 
-export default function game() {
+export default function Game() {
   return (
-    <div className="canvas-box">
-      <canvas width={950} height={950} id="mycanvas"></canvas>
+    <div className="game-container">
+      <video src={videoBg} autoPlay loop muted />
+
+      <div className="canvas-box">
+        <canvas width={950} height={950} id="mycanvas"></canvas>
+      </div>
     </div>
   );
 }
