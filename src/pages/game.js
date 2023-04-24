c3a0c62fb599784407c1582721d00467cc3adfba
PhilippRosie <122967743+PhilippRosie@users.noreply.github.com>
2023-04-24 14:06:33 +0200

    Update game.js

--- src/pages/game.js
+++ src/pages/game.js
@@ -4,7 +4,7 @@ import "../styles/game.css";
 export default function game() {
   return (
     <div className="canvas-box">
-      <canvas width={950} height={900} id="mycanvas"></canvas>
+      <canvas width={950} height={950} id="mycanvas"></canvas>
     </div>
   );
 }
