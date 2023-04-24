14973918dfddb54e61542763044077ff3bfb9f55
PhilippRosie <122967743+PhilippRosie@users.noreply.github.com>
2023-04-24 14:02:33 +0200

    small changes to game page

--- src/pages/game.js
+++ src/pages/game.js
@@ -4,7 +4,7 @@ import "../styles/game.css";
 export default function game() {
   return (
     <div className="canvas-box">
-      <canvas width={1000} height={1000} id="mycanvas"></canvas>
+      <canvas width={950} height={900} id="mycanvas"></canvas>
     </div>
   );
 }
