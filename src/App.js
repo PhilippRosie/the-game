12fe9ce7d62f301172b4e34d6e7355b92c7e7131
“noxar94” <Lundmark1223@hotmail.se>
2023-04-24 10:27:43 +0200

    removed logout

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Logout from "./pages/Logout";
 import Game from "./pages/game";
 
 function App() {
@@ -16,7 +15,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/logout" element={<Logout />} />
           <Route path="/game" element={<Game />} />
         </Routes>
       </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom";
 import React from "react";
 import "../styles/navbar.css";
 import "../pages/Login";
-import "../pages/Logout";
 import "../pages/game";
 export default function Navbar() {
   return (
@@ -11,7 +10,6 @@ export default function Navbar() {
       <nav>
         <Link to="/">Home</Link>
         <Link to="/login">Login</Link>
-        <Link to="/logout">Logout</Link>
         <Link to="/game">Game</Link>
       </nav>
     </header>
