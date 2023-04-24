32ea4e179d7cd9bd926574bd6ed5a9ad9419b93b
PhilippRosie <122967743+PhilippRosie@users.noreply.github.com>
2023-04-24 16:21:15 +0200

    Revert "added background to gamepage"
    This reverts commit 619b243f312f6befe57fe074aca78bbf98c35918.

--- src/pages/game.js
+++ src/pages/game.js
@@ -1,15 +1,10 @@
 import React from "react";
 import "../styles/game.css";
-import videoBg from "../assets/game-background.mp4";
 
-export default function Game() {
+export default function game() {
   return (
-    <div className="game-container">
-      <video src={videoBg} autoPlay loop muted />
-
-      <div className="canvas-box">
-        <canvas width={950} height={950} id="mycanvas"></canvas>
-      </div>
+    <div className="canvas-box">
+      <canvas width={950} height={950} id="mycanvas"></canvas>
     </div>
   );
 }
