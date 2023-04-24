69df5e02f113620e4d540a64c9bb9d8068546611
“noxar94” <Lundmark1223@hotmail.se>
2023-04-24 13:36:28 +0200

    made a canvas

--- src/pages/game.js
+++ src/pages/game.js
@@ -3,8 +3,8 @@ import "../styles/game.css";
 
 export default function game() {
   return (
-    <div>
-      <h1>GAME</h1>
+    <div className="canvas-box">
+      <canvas width={1000} height={1000} id="mycanvas"></canvas>
     </div>
   );
 }
