b7478a328c2da758603a3ab1a896806d45cc1379
“noxar94” <Lundmark1223@hotmail.se>
2023-04-25 15:17:06 +0200

    a

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  entry: "./index.js",
+  output: {
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {},
+          },
+        ],
+      },
+    ],
+  },
+};
