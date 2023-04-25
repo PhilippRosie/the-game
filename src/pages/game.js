f9835de3d18ff5db78faf6508bc2e8eeb861611c
“noxar94” <Lundmark1223@hotmail.se>
2023-04-25 14:59:53 +0200

    Snake done
    i've done the movement for the snake and made it look like a snake

--- src/pages/game.js
+++ src/pages/game.js
@@ -1,43 +1,119 @@
 import React from "react";
-import { select } from "d3";
+import { index, select } from "d3";
 import "../styles/game.css";
 
+import snake from "../img/Snake.jpg";
+
 // create N points such that adjecent nodes are L distance apart
-const createPoints = (n, L) => {
+const createPoints = (N, L) => {
   let x = 100;
   let y = 0;
   const points = [];
-  for (let i = 0; i < n; i += 1) {
+  for (let i = 0; i < N; i += 1) {
     points.push({ x, y });
     y += L;
   }
   return points;
 };
 
+const satisfyLinkConstraints = (points, L) => {
+  // head is unchanged
+  const newPoints = [points[0]];
+  for (let i = 1; i < points.length; i += 1) {
+    // satisfy distance Constraint
+    const a = newPoints[i - 1];
+    const b = points[i];
+    const dx = a.x - b.x;
+    const dy = a.y - b.y;
+    const distance = Math.hypot(dx, dy);
+    const vba = { x: dx / distance, y: dy / distance };
+    const x = a.x - L * vba.x;
+    const y = a.y - L * vba.y;
+    newPoints.push({ x, y });
+  }
+  return newPoints;
+};
+
 class Main extends React.Component {
   componentDidMount() {
-    this.N = 20;
-    this.L = 30;
+    this.N = 100; // points (how many divs)
+    this.L = 10; // gap between divs
     this.points = createPoints(this.N, this.L);
     this.draw();
-    this.down = false;
+    this.headAngle = -Math.PI / 2;
+    document.addEventListener("mousedown", (e) => {
+      this.down = true;
+      this.updateHead(e.clientX, e.clientY);
+    });
+    document.addEventListener("mousemove", (e) => {
+      if (!this.down) return;
+      this.updateHead(e.clientX, e.clientY);
+    });
+    document.addEventListener("mouseup", (e) => {
+      this.down = false;
+    });
+
+    setInterval(() => {
+      this.moveUp();
+    }, 15); // how fast the snake move
+
+    document.addEventListener("keydown", (e) => {
+      const k = e.key;
+      if (k === "ArrowLeft") {
+        this.headAngle -= 0.05;
+      }
+      if (k === "ArrowRight") {
+        this.headAngle += 0.05;
+      }
+    });
+  }
+
+  moveUp() {
+    const [head, ...tail] = this.points;
+    const del = 2;
+    const x = head.x + del * Math.cos(this.headAngle);
+    const y = head.y + del * Math.sin(this.headAngle);
+    this.updateHead(x, y);
+  }
+
+  updateHead(x, y) {
+    const [, ...tail] = this.points;
+    this.points = [{ x, y }, ...tail];
+    this.points = satisfyLinkConstraints(this.points, this.L);
+    this.draw();
   }
 
   draw() {
-    const width = 20;
     const node = select(this.node);
     node.style("position", "relative");
     const updateSel = node.selectAll("div").data(this.points);
+    const headWidth = 30;
+    const tailWidth = 10;
+    const noOfChunksForHead = 5;
+    const width = (index) => {
+      if (index < noOfChunksForHead) {
+        const peakWidth = 10;
+        const bigWidth = 35;
+        const f = index / (noOfChunksForHead - 1);
+        return peakWidth + f * (bigWidth - peakWidth);
+      }
+      return headWidth + (tailWidth - headWidth) * (index / (this.N - 1));
+    };
     updateSel
       .enter()
       .append("div")
       .style("position", "absolute")
-      .style("background", "white")
-      .style("width", `${width}px`)
-      .style("height", `${width}px`)
+      .style("background", (d, i) => {
+        const wid = 8;
+        const even = Math.floor(i / wid) % 2 === 0;
+        return even ? "white" : "green";
+      })
+      .style("border-radius", "50%")
+      .style("width", (d, i) => `${width(i)}px`)
+      .style("height", (d, i) => `${width(i)}px`)
       .merge(updateSel)
-      .style("left", (d) => `${d.x}px`)
-      .style("top", (d) => `${d.y}px`);
+      .style("left", (d, i) => `${d.x - width(i) / 2}px`)
+      .style("top", (d, i) => `${d.y - width(i) / 2}px`);
   }
 
   render() {
