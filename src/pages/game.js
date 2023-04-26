a9e1c985c121ccb35c8ab8dd020c9715d48e8e85
“noxar94” <Lundmark1223@hotmail.se>
2023-04-26 12:54:43 +0200

    small change

--- src/pages/game.js
+++ src/pages/game.js
@@ -1,12 +1,7 @@
 import React from "react";
-import { index, select } from "d3";
 import "../styles/game.css";
 import Snake from "../components/snake";
 
 export default function game() {
-  return (
-    <canvas width={1000} height={1000}>
-      <Snake />
-    </canvas>
-  );
+  return <Snake />;
 }
