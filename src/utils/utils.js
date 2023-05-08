8b374996558d4f868ca2eed277238dd863f19431
“noxar94” <Lundmark1223@hotmail.se>
2023-05-08 15:30:03 +0200

    alot..

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,17 +1,8 @@
-Array.prototype.parse2D = function () {
-  const rows = [];
-  for (let i = 0; i < this.length; i += 16) {
-    rows.push(this.slice(i, i + 16));
-  }
-
-  return rows;
-};
-
 Array.prototype.createObjectsFrom2D = function () {
   const objects = [];
   this.forEach((row, y) => {
     row.forEach((symbol, x) => {
-      if (symbol === 292 || symbol === 250) {
+      if (symbol === 1480 || symbol === 1448) {
         // push a new collision into collisionblocks array
         objects.push(
           new collisionBlock({
