24fd56045bbfd90e63ba6305d22ed4ff6e80a7d9
“noxar94” <Lundmark1223@hotmail.se>
2023-05-08 13:26:50 +0200

    fuckit...

--- src/pages/game.js
+++ src/pages/game.js
@@ -1,15 +1,6 @@
 import React from "react";
 import "../styles/game.css";
-import Canvas from "../components/canvas";
-import { color } from "framer-motion";
 
 export default function game() {
-  return (
-    <div>
-      <Canvas></Canvas>
-      <div className="Instructions">
-        <p>Move with W,S,A,D</p>
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
