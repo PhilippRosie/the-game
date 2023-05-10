f8551c1ff7909a8a0e07939319bbbe0f2036c4c0
PhilippRosie <122967743+PhilippRosie@users.noreply.github.com>
2023-05-10 09:44:13 +0200

    added user page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,6 +4,7 @@ import "../styles/navbar.css";
 import "../pages/profile";
 import CreateAcc from "./createAcc";
 import Login from "./login";
+import { authenticate } from "../helpers";
 
 import "../pages/game";
 
@@ -14,6 +15,8 @@ export default function Navbar() {
     setCurrentForm(forName);
   };
 
+  const auth = authenticate();
+
   return (
     <header>
       <h3 className="logo">The GAME.</h3>
@@ -21,6 +24,7 @@ export default function Navbar() {
         <div className="main-menu">
           <Link to="/">Home</Link>
           <Link to="/game">Game</Link>
+          {auth ? <Link to="/userPage">User Page</Link> : null}
         </div>
         <div className="user-menu">
           {currentForm === "login" ? (
