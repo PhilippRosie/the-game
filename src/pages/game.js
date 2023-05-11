e09ea2116642fa72958fda96082ecf79bab174ce
“noxar94” <Lundmark1223@hotmail.se>
2023-05-11 14:47:47 +0200

    removed..

--- src/pages/game.js
+++ src/pages/game.js
@@ -1,11 +1,6 @@
 import React from "react";
 import "../styles/game.css";
-import Board from "../components/gameBoard";
 
 export default function Game() {
-  return (
-    <div>
-      <Board />
-    </div>
-  );
+  return <div></div>;
 }
