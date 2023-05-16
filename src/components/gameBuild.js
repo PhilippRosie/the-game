7d1b3c6c8ef0c4722d705f4aac5c3336dac97284
PhilippRosie <122967743+PhilippRosie@users.noreply.github.com>
2023-05-16 11:52:49 +0200

    added funtion start with overlay, reload with overlay, sets everything back to beggining, gameover function for scores and to reload

--- src/components/gameBuild.js
+++ src/components/gameBuild.js
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/gameBuild.css";
 
 const GameBuild = () => {
   const [heads, setHeads] = useState([]);
+  const [startGame, setStartGame] = useState(false);
+  const [showStartBtn, setShowStartBtn] = useState(true);
   const [clickedHeads, setClickedHeads] = useState(0);
+  const [timeLeft, setTimeLeft] = useState(20);
+  const [gameOver, setGameOver] = useState(false);
 
   const makeHead = () => {
     const gameHead = {
@@ -22,12 +26,45 @@ const GameBuild = () => {
 
   const startBtn = () => {
     makeHead();
+    setStartGame(true);
+    setShowStartBtn(false);
+  };
+
+  useEffect(() => {
+    let countDown;
+
+    if (startGame) {
+      timeLeft > 0
+        ? (countDown = setInterval(() => {
+            setTimeLeft((timeBefore) => timeBefore - 1);
+          }, 1000))
+        : setGameOver(true);
+    }
+
+    return () => {
+      clearInterval(countDown);
+    };
+  }, [timeLeft, startGame]);
+
+  const reloadGame = () => {
+    setHeads([]);
+    setClickedHeads(0);
+    setTimeLeft(20);
+    setGameOver(false);
+    makeHead();
+    setShowStartBtn(false);
   };
 
   return (
     <div className="gameContainer">
       <div>
-        <p className="smashHeads">Smashed Trumps:{clickedHeads} </p>
+        <div className="gameHeading">
+          <p className="smashHeads">Smashed Trumps:{clickedHeads} </p>
+          <p className="timerContainer">
+            Time Left: <span id="time">{timeLeft}</span>
+          </p>
+        </div>
+
         <div className="gameBoard">
           {heads.map((head) => (
             <div
@@ -39,10 +76,25 @@ const GameBuild = () => {
           ))}
         </div>
       </div>
-      <button className="startGameBtn" onClick={startBtn}>
-        Start Game
-      </button>
+      {showStartBtn && (
+        <div className="startGameContainer overlay">
+          <button className="startGameBtn " onClick={startBtn}>
+            Start Game
+          </button>
+        </div>
+      )}
+
+      {gameOver && (
+        <div className="resultGameOverContainer overlay">
+          <h3 className="gameOverText"> Game Over! </h3>
+          <p className="score">Score: {clickedHeads}</p>
+          <button onClick={reloadGame} className="reloadBtn">
+            Let's do it AGAIN!
+          </button>
+        </div>
+      )}
     </div>
   );
 };
+
 export default GameBuild;
