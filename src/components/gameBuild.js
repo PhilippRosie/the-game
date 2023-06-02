15d7197ca756b346f18ad367a522682ac64d1aab
PhilippRosie <122967743+PhilippRosie@users.noreply.github.com>
2023-06-02 13:46:21 +0200

    added baam effect when hitting head and added missed effect when misseed the head

--- src/components/gameBuild.js
+++ src/components/gameBuild.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect, useCallback } from "react";
 import "../styles/gameBuild.css";
 
+import baam from "../images/BAAM.png";
+import missed from "../images/MISSED.png";
+
 const GameBuild = () => {
   const [heads, setHeads] = useState([]);
   const [startGame, setStartGame] = useState(false);
@@ -8,6 +11,9 @@ const GameBuild = () => {
   const [clickedHeads, setClickedHeads] = useState(0);
   const [timeLeft, setTimeLeft] = useState(20);
   const [gameOver, setGameOver] = useState(false);
+
+  const [showBaam, setShowBaam] = useState(false);
+  const [showMissed, setShowMissed] = useState(false);
 
   const makeHead = useCallback(() => {
     const gameHead = {
@@ -23,6 +29,22 @@ const GameBuild = () => {
     setClickedHeads(clickedHeads + 1);
     setTimeLeft((addedTime) => addedTime + 1);
     makeHead();
+  };
+
+  const handleBaam = () => {
+    setShowBaam(true);
+
+    setTimeout(() => {
+      setShowBaam(false);
+    }, 500);
+  };
+
+  const handleMissed = () => {
+    setShowMissed(true);
+
+    setTimeout(() => {
+      setShowMissed(false);
+    }, 500);
   };
 
   const removeTime = () => {
@@ -67,7 +89,12 @@ const GameBuild = () => {
   };
 
   return (
-    <div className="gameContainer" onClick={removeTime}>
+    <div
+      className="gameContainer"
+      onClick={() => {
+        removeTime();
+      }}
+    >
       <div>
         <div className="gameHeading">
           <p className="smashHeads">Smashed Trumps:{clickedHeads} </p>
@@ -76,14 +103,29 @@ const GameBuild = () => {
           </p>
         </div>
 
-        <div className="gameBoard">
+        <div
+          className="gameBoard"
+          onClick={(e) => {
+            if (e.target.classList.contains("gameBoard")) {
+              handleMissed();
+            }
+          }}
+        >
           {heads.map((head) => (
             <div
               key={head.id}
-              onClick={() => removeHead(head.id)}
+              onClick={() => {
+                removeHead(head.id);
+                handleBaam();
+              }}
               className="politician"
               style={{ left: head.left + "700px", top: head.top + "700px" }}
-            ></div>
+            >
+              {showBaam && <img className="baamImage" src={baam} alt="BAAM" />}
+              {showMissed && (
+                <img className="missedImage" src={missed} alt="MISSED" />
+              )}
+            </div>
           ))}
         </div>
       </div>
